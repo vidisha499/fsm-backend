db7a9c607918544ed10467cb3d5d033d60c5eea1
Eshika Pakhale <eshikapakhale@gmail.com>
2026-02-06 09:22:27 +0530

    fix: updated neon ssl connection and attendance entity

--- src/attendance/dto/create-attendance.dto.ts
+++ src/attendance/dto/create-attendance.dto.ts
@@ -1,6 +1,15 @@
+// export class CreateAttendanceDto {
+//   rangerId: number;
+//   photo?: string;
+//   ranger?: string;
+//   geofence?: string;
+// }
+
 export class CreateAttendanceDto {
-  rangerId: number;
-  photo?: string;
-  ranger?: string;
-  geofence?: string;
+  ranger_id: number;
+  rangerName: string;
+  region: string;
+  geofence: string;
+  type: string;
+  photo: string;
 }
